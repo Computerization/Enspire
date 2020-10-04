7f44db0f1ee942da08b55996406541c5723a744b
Josh-Cena <joshcena@qq.com>
2020-10-04 13:40:08 +0800

    Add album layout

--- src/App.vue
+++ src/App.vue
@@ -47,19 +47,14 @@
       </v-list>
     </v-navigation-drawer>
     <v-main>
-      <router-view />
+      <v-container fluid>
+        <router-view />
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <style scoped>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-
 .logo {
   border-radius: 20%;
   background-color: white;
