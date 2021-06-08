869058d6292a635425b30422dd7f496d67beee29
Josh-Cena <sidachen2003@gmail.com>
2021-06-08 09:07:48 +0800

    Add SamChou in contributors

--- src/views/About.vue
+++ src/views/About.vue
@@ -37,14 +37,19 @@
           </v-card>
         </v-col>
         <v-col>
-          <v-card class="mx-auto" width="270" outlined>
+          <v-card
+            class="mx-auto"
+            width="270"
+            color="rgba(0,128,255,0.1)"
+            outlined
+          >
             <v-list-item three-line>
               <v-list-item-content>
                 <div class="overline mb-4">社区贡献者</div>
                 <v-list-item-title class="headline mb-1">
                   贡献者 X
                 </v-list-item-title>
-                <v-list-item-subtitle> 会是你吗？ </v-list-item-subtitle>
+                <v-list-item-subtitle>会是你吗？</v-list-item-subtitle>
               </v-list-item-content>
               <v-list-item-avatar size="100">
                 <img
@@ -53,7 +58,7 @@
                 />
               </v-list-item-avatar>
             </v-list-item>
-            <v-card-actions class="white justify-center">
+            <v-card-actions class="justify-center">
               <v-btn
                 text
                 href="https://github.com/computerization/enspire"
@@ -74,61 +79,64 @@
 import Vue from "vue";
 export default Vue.extend({
   name: "about",
-  data() {
-    return {
-      contributors: [
-        {
-          alias: "Josh-Cena",
-          name: "陈思达",
-          desc: "22届Web部负责人",
-        },
-        {
-          alias: "yechs",
-          name: "舒烨",
-          desc: "20届社长",
-        },
-        {
-          alias: "JoyceQu",
-          name: "曲乐成",
-          desc: "22届美工",
-        },
-        {
-          alias: "Will-WHT",
-          name: "王昊天",
-          desc: "22届社长",
-        },
-        {
-          alias: "ETwilight",
-          name: "郭培扬",
-          desc: "22届副社长",
-        },
-        {
-          alias: "edgexyz",
-          name: "杨书",
-          desc: "22届后端（伪）",
-        },
-        {
-          alias: "Lazenander",
-          name: "杨淏然",
-          desc: "23届打工人",
-        },
-        {
-          alias: "GeMini-220",
-          name: "张驰",
-          desc: "23届社员",
-        },
-        {
-          alias: "mathscty",
-          name: "陈韬宇",
-          desc: "23届开发者",
-        },
-        {
-          alias: "KevJ12",
-          name: "姜奕辰",
-          desc: "23届社员",
-        },
-      ],
-    };
-  },
+  data: () => ({
+    contributors: [
+      {
+        alias: "Josh-Cena",
+        name: "陈思达",
+        desc: "22届Web负责人",
+      },
+      {
+        alias: "ETwilight",
+        name: "郭培扬",
+        desc: "22届副社长",
+      },
+      {
+        alias: "Lazenander",
+        name: "杨淏然",
+        desc: "23届打工人",
+      },
+      {
+        alias: "GeMini-220",
+        name: "张驰",
+        desc: "23届社员",
+      },
+      {
+        alias: "SamChou19815",
+        name: "周天祎",
+        desc: "17届社长",
+      },
+      {
+        alias: "mathscty",
+        name: "陈韬宇",
+        desc: "23届开发者",
+      },
+      {
+        alias: "KevJ12",
+        name: "姜奕辰",
+        desc: "23届社员",
+      },
+      {
+        alias: "yechs",
+        name: "舒烨",
+        desc: "20届社长",
+      },
+      {
+        alias: "JoyceQu",
+        name: "曲乐成",
+        desc: "22届美工",
+      },
+      {
+        alias: "edgexyz",
+        name: "杨书",
+        desc: "22届后端（伪）",
+      },
+      {
+        alias: "Will-WHT",
+        name: "王昊天",
+        desc: "22届社长",
+      },
+    ],
+  }),
 });
 </script>
