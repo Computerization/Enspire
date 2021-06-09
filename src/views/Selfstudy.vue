50ab9ce550771824720f5d402c106513e30f4904
Josh-Cena <sidachen2003@gmail.com>
2021-06-09 13:27:07 +0800

    Add placeholders

---
+++ src/views/Selfstudy.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container>
+    <h1>提交请假单</h1>
+    <v-row>
+      <v-col>
+        <v-text-field></v-text-field>
+      </v-col>
+      <v-col>
+        <v-text-field></v-text-field>
+      </v-col>
+      <v-col>
+        <v-text-field></v-text-field>
+      </v-col>
+      <v-col>
+        <v-text-field></v-text-field>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Selfstudy extends Vue {}
+</script>
