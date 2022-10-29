aec2778ad16e5a9cb3f3b90a87d6913142f44b0b
phoeniiix1203 <guqiyuan001@gmail.com>
2022-10-29 11:15:05 +0800

    feat: Update readme

--- public/static/Request.js
+++ public/static/Request.js
@@ -1,27 +1,20 @@
-function GetDate()
-{
-    var d = new Date();
-document.getElementById("demo").innerHTML = d.getDay();
+function GetDate() {
+  var d = new Date();
+  document.getElementById("demo").innerHTML = d.getDay();
 }
-
 
 var n = localStorage.getItem("date");
-document.getElementById("showDate").innerHTML=n;
+document.getElementById("showDate").innerHTML = n;
 
-function getInput()
-{
-    
+function getInput() {}
+
+function SaveData() {
+  var st = document.getElementById("Reason");
+  localStorage.setItem("Reasons", st.value);
+  write(st);
 }
 
-function SaveData()
-{
-    var st=document.getElementById("Reason");
-    localStorage.setItem("Reasons",st.value);
-    write(st);
-}
-
-function ShowData()
-{
-    var s=localStorage.getItem("Reasons");
-document.getElementById("demo").innerHTML=s;
+function ShowData() {
+  var s = localStorage.getItem("Reasons");
+  document.getElementById("demo").innerHTML = s;
 }
--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import store from "./store";
 import vuetify from "./plugins/vuetify";
 import "./axios";
 
-
 Vue.config.productionTip = false;
 
 new Vue({
