973fbc5ed050217a4d530eedcdbcd0cff021ebf6
qwerzl <me@qwerzl.me>
2024-02-05 17:31:09 +0800

    chore: don't let clerk's userid to be the primary key for the user schema
    bad string, bad

--- db/migrations/20240205092944_init/migration.sql
+++ db/migrations/20240205092944_init/migration.sql
@@ -1,6 +1,7 @@
 -- CreateTable
 CREATE TABLE "User" (
-    "id" TEXT NOT NULL,
+    "id" SERIAL NOT NULL,
+    "clerkUserId" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "tsimsStudentId" INTEGER NOT NULL,
 
@@ -19,6 +20,9 @@ CREATE TABLE "Club" (
 
     CONSTRAINT "Club_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_clerkUserId_key" ON "User"("clerkUserId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_tsimsStudentId_key" ON "User"("tsimsStudentId");
