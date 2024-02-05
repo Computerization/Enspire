68d62c10060948f2d1a4af3ac43242c6c9df328d
qwerzl <me@qwerzl.me>
2024-02-05 18:26:59 +0800

    feat: add backend api to get all clubs of a user

---
+++ server/api/user/all_clubs.get.ts
@@ -0,0 +1,53 @@
+import {PrismaClient} from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export default eventHandler(async (event) => {
+  const { auth } = event.context
+  console.log(auth)
+
+  if (!auth.userId) {
+    setResponseStatus(event, 403)
+    return
+  }
+
+  const tsimsStudentId = (await prisma.user.findUniqueOrThrow({
+    where: {
+      clerkUserId: auth.userId
+    }
+  })).tsimsStudentId
+
+  return {
+    "president": await prisma.club.findMany({
+      where: {
+        presidentByTsimsStudentId: tsimsStudentId
+      },
+      select: {
+        id: true,
+        name: true
+      }
+    }),
+    "vice": await prisma.club.findMany({
+      where: {
+        vicesByTsimsStudentId: {
+          has: tsimsStudentId
+        },
+      },
+      select: {
+        id: true,
+        name: true
+      }
+    }),
+    "member": await prisma.club.findMany({
+      where: {
+        membersByTsimsStudentId: {
+          has: tsimsStudentId
+        },
+      },
+      select: {
+        id: true,
+        name: true
+      }
+    }),
+  }
+})
