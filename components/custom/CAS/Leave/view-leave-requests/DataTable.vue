aa7e2c9a42f400436f21bb8bc9d07c55cd80bdab
qwerzl <me@qwerzl.me>
2024-02-24 00:17:17 +0800

    feat: complete `ViewLeaveRequests` support
    i forgor

--- components/custom/CAS/Leave/view-leave-requests/DataTable.vue
+++ components/custom/CAS/Leave/view-leave-requests/DataTable.vue
@@ -9,10 +9,10 @@ import {
   useVueTable,
 } from '@tanstack/vue-table'
 
-import type { Club, LeaveRequest } from '@prisma/client'
+import type { Club, LeaveRequest, User } from '@prisma/client'
 import { valueUpdater } from '@/lib/utils'
 import { Input } from '@/components/ui/input'
-
+import { Badge } from '~/components/ui/badge'
 import {
   Table,
   TableBody,
@@ -25,9 +25,10 @@ import {
 const props = defineProps<{
   columns: ColumnDef<TData, TValue>[]
   data: TData[]
+  refreshFunction: Function
 }>()
 
-type LeaveRequestWithClubInfo = LeaveRequest & { club: Club }
+type LeaveRequestWithClubInfo = LeaveRequest & { club: Club, user: User }
 
 const isDialogOpen = ref(false)
 const sorting = ref<SortingState>([])
@@ -48,6 +49,25 @@ const table = useVueTable({
     get columnFilters() { return columnFilters.value },
   },
 })
+
+const isLoading = ref(false)
+const deleteDialogOpen = ref(false)
+
+table.setPageSize(5)
+
+async function submitDeletion(id: number) {
+  isLoading.value = true
+  await useFetch('/api/cas/leave/delete', {
+    method: 'DELETE',
+    body: {
+      id,
+    },
+  })
+  isLoading.value = true
+  deleteDialogOpen.value = false
+  isDialogOpen.value = false
+  props.refreshFunction()
+}
 </script>
 
 <template>
@@ -123,14 +143,74 @@ const table = useVueTable({
 
       <div>
         <div class="font-medium">
+          状态
+        </div>
+        <div v-if="currentRequestInDialog?.status === 'PENDING'" class="mt-1 bg-yellow-100 text-yellow-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-yellow-900 dark:text-yellow-300 w-[70px]">
+          等待审批
+        </div>
+        <Badge v-if="currentRequestInDialog?.status === 'APPROVED'" class="mt-1 bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300 w-[70px]">
+          审批通过
+        </Badge>
+        <Badge v-if="currentRequestInDialog?.status === 'DENIED'" class="mt-1 bg-red-100 text-red-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-red-300 w-[70px]" color="orange">
+          被拒绝
+        </Badge>
+      </div>
+
+      <div>
+        <div class="font-medium">
           日期
         </div>
         <div class="font-mono text-muted-foreground text-sm italic">
           {{ new Date(String(currentRequestInDialog?.startDate)).toLocaleDateString() }}
         </div>
       </div>
+
+      <div>
+        <div class="font-medium">
+          申请者
+        </div>
+        <div class="font-mono text-muted-foreground text-sm italic">
+          {{ currentRequestInDialog?.user.name }}
+        </div>
+      </div>
+
+      <div>
+        <div class="font-medium">
+          原因
+        </div>
+        <div class="font-mono text-muted-foreground text-sm italic">
+          {{ currentRequestInDialog?.reason }}
+        </div>
+      </div>
+
       <DialogFooter>
-        Save changes
+        <Dialog v-model:open="deleteDialogOpen">
+          <DialogTrigger as-child>
+            <Button class="mt-1" size="sm">
+              <span class="sr-only">Open menu</span>
+              <Icon name="material-symbols:delete-outline" size="1.5em" />
+            </Button>
+          </DialogTrigger>
+          <DialogContent class="sm:max-w-md">
+            <DialogHeader>
+              <DialogTitle>您确定吗？</DialogTitle>
+            </DialogHeader>
+            <div class="text-muted-foreground text-sm">
+              该操作无法撤消。
+            </div>
+            <DialogFooter class="justify-end">
+              <DialogClose as-child>
+                <Button type="button" variant="secondary" :disabled="isLoading">
+                  Close
+                </Button>
+              </DialogClose>
+              <Button v-if="currentRequestInDialog?.id" variant="destructive" type="button" :disabled="isLoading" @click="submitDeletion(currentRequestInDialog?.id)">
+                <Icon v-if="isLoading" class="mr-2" name="svg-spinners:180-ring-with-bg" />
+                确定
+              </Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
       </DialogFooter>
     </DialogContent>
   </Dialog>
