17f37ab37dd6c481b781cadd2d828fddb85f9d85
qwerzl <me@qwerzl.me>
2024-02-28 08:54:25 +0800

    fix: make `vue-clerk/plugin` happy
    sorry, typo in the last commit message, feature.

--- plugins/clerk.ts
+++ plugins/clerk.ts
@@ -1,14 +1,7 @@
 import { clerkPlugin } from 'vue-clerk/plugin'
 
 export default defineNuxtPlugin(async (nuxtApp) => {
-  const publishableKey = useRuntimeConfig().public.clerkPublishableKey as string
-
   nuxtApp.vueApp.use(clerkPlugin, {
-    publishableKey,
-    options: {
-      appearance: {
-        variables: { colorPrimary: '#570DF8' },
-      },
-    },
+    publishableKey: useRuntimeConfig().public.clerkPublishableKey,
   })
 })
