9338c57a6510cc0c5a3846558e432a70bc74e646
qwerzl <me@qwerzl.me>
2024-02-29 08:29:30 +0800

    fix: let prisma generate client

--- plugins/sentry.client.ts
+++ plugins/sentry.client.ts
@@ -17,7 +17,6 @@ export default defineNuxtPlugin((nuxtApp) => {
     ],
 
     // Configure this whole part as you need it!
-
     tracesSampleRate: 0.2, // Change in prod
 
     // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
