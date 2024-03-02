6106e4ff147cc6c0ac20fa6d3166b8e39bfc0586
Alan Ye <45558679+AlanYe-Dev@users.noreply.github.com>
2024-03-02 18:26:36 +0800

    fix: return `''` if `!content`

--- components/custom/cleanHTML.d.ts
+++ components/custom/cleanHTML.d.ts
@@ -2,6 +2,10 @@ export function cleanHTML(content: string): string
 
 // Define
 export function cleanHTML(content: string): string {
+  if (!content) {
+    return '';
+  }
+
   return content.replace(/<script[^>]*>([\s\S]*?)<\/script>/gmi, '')
     .replace(/<style[^>]*>([\s\S]*?)<\/style>/gmi, '')
 }
