7e332dabd4b81a9e09672f104af0e8508e3bc362
Alan Ye <45558679+AlanYe-Dev@users.noreply.github.com>
2024-03-02 18:32:13 +0800

    style: merge into single line

--- components/custom/cleanHTML.d.ts
+++ components/custom/cleanHTML.d.ts
@@ -2,9 +2,7 @@ export function cleanHTML(content: string): string
 
 // Define
 export function cleanHTML(content: string): string {
-  if (!content) {
-    return '';
-  }
+  if (!content) return ''
 
   return content.replace(/<script[^>]*>([\s\S]*?)<\/script>/gmi, '')
     .replace(/<style[^>]*>([\s\S]*?)<\/style>/gmi, '')
