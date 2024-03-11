77177221f7684b9618a586fb560b4caae0e01d16
q1zhen <142765265+q1zhen@users.noreply.github.com>
2024-03-11 22:57:26 +0800

    feat: added content for about page

--- content/about.md
+++
@@ -1,6 +0,0 @@
-# 关于我们
-
-TODO
-
-# 更新日志
-<ContentDoc path="/changelog" />
--- pages/about.vue
+++ pages/about.vue
@@ -5,9 +5,22 @@ definePageMeta({
 </script>
 
 <template>
-  <!-- Content source at /content/about.md -->
-  <div class="prose prose-invert prose-h1:mb-1 prose-h2:mt-5">
-    <ContentDoc path="/about" />
+  <div class="prose prose-invert prose-h1:mb-1 prose-h2:mt-5 max-w-full">
+    <div class="inline-block h-full align-top lg:mr-24">
+      <!-- /content/about/enspire.md -->
+      <ContentDoc path="/about/enspire" class="mb-12" />
+      <!-- /content/about/computerization.md -->
+      <ContentDoc path="/about/computerization" class="mb-12" />
+    </div>
+    <div class="inline-block align-top h-full min-w-72">
+      <h1 class="my-0">
+        更新日志
+      </h1>
+      <div class="overflow-y-scroll my-2" style="max-height: 80vh;">
+        <!-- /content/about/changelog.md -->
+        <ContentDoc path="/about/changelog" />
+      </div>
+    </div>
   </div>
 </template>
 
