3d9360a784ffeec3b56be861910e09bad2655847
Alan Ye <45558679+at-wr@users.noreply.github.com>
2024-03-11 16:34:38 +0800

    feat: add club member list (#519)
    * feat: add club member list
    
    * fix(ui): better ui layout
    
    ---------
    
    Co-authored-by: qwerzl <me@qwerzl.me>

--- pages/cas/clubs/[id].vue
+++ pages/cas/clubs/[id].vue
@@ -65,14 +65,43 @@ definePageMeta({
               </CardDescription>
             </CardHeader>
             <CardContent>
-              <div v-if="hasDescriptionC" v-text="Description_C" />
-              <div v-else class="text-sm italic text-muted-foreground text-center w-full">
+              <div class="font-bold">
+                简介
+              </div>
+              <div v-if="hasDescriptionC" class="my-3" v-text="Description_C" />
+              <div v-else class="text-sm italic text-muted-foreground text-center w-full my-2">
                 暂无简介 ;-(
               </div>
               <!-- Don't show the English Description until i18n is completed -->
+              <Separator class="my-4" />
+              <div class="font-bold">
+                成员
+              </div>
+              <div v-if="club.gmember.length === 0" class="text-sm italic text-muted-foreground text-center w-full my-2">
+                暂无成员 ;-(
+              </div>
+              <div v-else class="mt-3">
+                <div class="flex flex-wrap">
+                  <div v-for="(member, index) in club.gmember" :key="member.StudentID" class="flex items-center">
+                    <div class="flex items-center">
+                      <span class="">{{ member.S_Name }}</span> <span class="text-muted-foreground">({{ member.S_Nickname }})</span>
+                      <Badge v-if="Number(member.LeaderYes) === 2" variant="default" class="ml-1">
+                        社长
+                      </Badge>
+                      <Badge v-else-if="Number(member.LeaderYes) === 1" variant="secondary" class="ml-1">
+                        副社
+                      </Badge>
+                      <Badge v-else variant="outline" class="ml-1">
+                        社员
+                      </Badge>
+                      <span v-if="index < club.gmember.length - 1" class="mx-2">/</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </CardContent>
           </Card>
-          <Card class="xl:w-1/4 w-full xl:ml-2">
+          <Card class="xl:w-1/4 w-full xl:ml-2 h-min">
             <CardHeader>
               <CardTitle class="flex items-center h-min gap-x-1">
                 社团属性
