12e8892b0fcdda8736345f9bc95e15d179d57d28
Alan Ye <45558679+at-wr@users.noreply.github.com>
2024-03-15 09:31:29 +0800

    feat(clubList): search (#518)
    * feat: search for club in either English or Chinese
    
    * fix(ui): small screens
    
    * chore: move from `regex` to `include`
    
    * Revert "fix(ui): small screens"
    
    This reverts commit 7f32f1bc4a97ed42091ba82fb793556fe3cb4c83.
    
    * fix(ui): fix ui on small screens
    
    ---------
    
    Co-authored-by: qwerzl <me@qwerzl.me>

--- pages/cas/clubs/index.vue
+++ pages/cas/clubs/index.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
+import { Input } from '@/components/ui/input'
 import TabsList from '@/components/ui/tabs/TabsList.vue'
 import Tabs from '@/components/ui/tabs/Tabs.vue'
 import TabsContent from '@/components/ui/tabs/TabsContent.vue'
 import TabsTrigger from '@/components/ui/tabs/TabsTrigger.vue'
 import json from '@/content/clubs.json'
 
-import type { ClubCategoryKey, Clubs } from '@/content/clubs'
+import type { ClubCategoryKey, Clubs, Groups } from '@/content/clubs'
 import ClubCard from '@/components/custom/club-card.vue'
 
 const clubs: Clubs = json as Clubs
@@ -16,29 +17,69 @@ definePageMeta({
 })
 
 const categories = (['Sports', 'Service', 'Arts', 'Life', 'Academic'] as const).map(c => c as ClubCategoryKey)
+
+const searchTerm = ref('')
+const allClubs = computed(() => Object.values(clubs).flat())
+
+const filteredClubs = computed(() => {
+  // return all clubs if no search term
+  if (!searchTerm.value)
+    return clubs
+
+  // ignore capitalization
+  const lowerCaseSearchTerm = searchTerm.value.toLowerCase()
+
+  // return zh/en name match the search term
+  return Object.entries(clubs).reduce((acc, [category]) => {
+    acc[category as ClubCategoryKey] = allClubs.value.filter(club =>
+      club.groups.some((group: Groups) =>
+        (group.C_NameC as string).toLowerCase().includes(lowerCaseSearchTerm) || (group.C_NameE as string).toLowerCase().includes(lowerCaseSearchTerm),
+      ),
+    )
+    return acc
+  }, {} as Clubs)
+})
+
+const isSearchActive = computed(() => searchTerm.value !== '')
 </script>
 
 <template>
   <div>
-    <Tabs class="h-full space-y-6" default-value="Sports">
-      <div class="space-between flex items-center">
-        <TabsList>
-          <TabsTrigger value="Sports">
-            Sports
+    <Tabs class="h-full space-y-6" :value="isSearchActive ? '' : undefined" :default-value="isSearchActive ? '' : 'Sports'">
+      <div class="flex flex-row items-center justify-between">
+        <TabsList class="w-min">
+          <TabsTrigger :value="isSearchActive ? '' : 'Sports'" :disabled="isSearchActive" class="flex items-center">
+            <Icon name="material-symbols:sports-basketball" />
+            <div class="hidden sm:block ml-1">
+              Sports
+            </div>
           </TabsTrigger>
-          <TabsTrigger value="Service">
-            Service
+          <TabsTrigger :value="isSearchActive ? '' : 'Service'" :disabled="isSearchActive" class="flex items-center">
+            <Icon name="material-symbols:home-repair-service" />
+            <div class="hidden sm:block ml-1">
+              Service
+            </div>
           </TabsTrigger>
-          <TabsTrigger value="Arts">
-            Arts
+          <TabsTrigger :value="isSearchActive ? '' : 'Arts'" :disabled="isSearchActive" class="flex items-center">
+            <Icon name="material-symbols:format-paint" />
+            <div class="hidden sm:block ml-1">
+              Arts
+            </div>
           </TabsTrigger>
-          <TabsTrigger value="Life">
-            Life
+          <TabsTrigger :value="isSearchActive ? '' : 'Life'" :disabled="isSearchActive" class="flex items-center">
+            <Icon name="material-symbols:nightlife" />
+            <div class="hidden sm:block ml-1">
+              Life
+            </div>
           </TabsTrigger>
-          <TabsTrigger value="Academic">
-            Academic
+          <TabsTrigger :value="isSearchActive ? '' : 'Academic'" :disabled="isSearchActive" class="flex items-center">
+            <Icon name="material-symbols:book" />
+            <div class="hidden sm:block ml-1">
+              Academic
+            </div>
           </TabsTrigger>
         </TabsList>
+        <Input v-model="searchTerm" type="text" placeholder="Search..." class="float-right w-1/4" />
       </div>
       <TabsContent
         v-for="i in categories"
@@ -47,7 +88,7 @@ const categories = (['Sports', 'Service', 'Arts', 'Life', 'Academic'] as const).
         class="border-none p-0 outline-none"
       >
         <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
-          <ClubCard v-for="j in clubs[i]" :key="j.groups[0].C_NameE" :club="j" />
+          <ClubCard v-for="j in filteredClubs[i]" :key="j.groups[0].C_NameE" :club="j" />
         </div>
       </TabsContent>
     </Tabs>
