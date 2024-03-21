4ed02be68c618ba298b73769202de5dc01bff83b
Alan Ye <45558679+AlanYe-Dev@users.noreply.github.com>
2024-03-21 19:22:05 +0800

    feat(API): added delete club invitation data

---
+++ server/api/cas/group/delete.delete.ts
@@ -0,0 +1,60 @@
+import { PrismaClient } from '@prisma/client'
+import * as z from 'zod'
+
+const prisma = new PrismaClient()
+
+const requestSchema = z.object({
+  clubId: z.string(),
+})
+
+export default eventHandler(async (event) => {
+  const { auth } = event.context
+
+  if (!auth.userId) {
+    setResponseStatus(event, 403)
+    return
+  }
+
+  const tsimsStudentId = await prisma.user.findUnique({
+    where: {
+      id: auth.userId,
+    },
+  }).then(user => user?.tsimsStudentId)
+
+  const result = await readValidatedBody(event, body => requestSchema.safeParse(body))
+
+  if (!result.success) {
+    setResponseStatus(event, 400)
+    return
+  }
+
+  const requestBody = result.data
+
+  // check if the user is in the presidentByTsimsStudentId or vicePresidentByTsimsStudentId of the club
+  const isPresidentOrVicePresident = await prisma.club.findFirst({
+    where: {
+      id: Number(requestBody.clubId),
+      OR: [
+        {
+          presidentByTsimsStudentId: tsimsStudentId,
+        },
+        {
+          vicesByTsimsStudentId: {
+            has: tsimsStudentId,
+          },
+        },
+      ],
+    },
+  }).then(club => club !== null)
+
+  if (!isPresidentOrVicePresident)
+    setResponseStatus(event, 403)
+
+  await prisma.joinClub.delete({
+    where: {
+      clubId: Number(requestBody.clubId),
+    },
+  }).catch(() => {
+    setResponseStatus(event, 404)
+  })
+})
