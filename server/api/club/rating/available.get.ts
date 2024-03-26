9a7d182e3b3f3a00ff7ba72b166b6b4673696069
Tom Tang <me@qwerzl.me>
2024-03-26 21:04:55 +0800

    Update available.get.ts

--- server/api/club/rating/available.get.ts
+++ server/api/club/rating/available.get.ts
@@ -13,9 +13,9 @@ export default eventHandler(async (event) => {
   const now = new Date()
   let semester = now.getFullYear().toString() // string for storing scope: yyyy[a|b]
 
-  if (([0, 1]).includes(now.getMonth()))
+  if (([1, 2]).includes(now.getMonth()))
     semester += 'a' // the `a` period of year
-  else if (([5, 6]).includes(now.getMonth()))
+  else if (([6, 7]).includes(now.getMonth()))
     semester += 'b' // the `b` period of year
   else
     return [] // one should ensure that this api will never be called when the current month is not in [0, 1, 5, 6]
