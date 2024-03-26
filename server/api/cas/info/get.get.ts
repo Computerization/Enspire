fd1c91a87d3dffddc90a8fb2d35eac5292565d0e
Alan Ye <45558679+at-wr@users.noreply.github.com>
2024-03-26 23:10:17 +0800

    feat(API): club info api (#536)
    * feat(API): add APIs for setting invitation links of Club Group
    
    * feat(Prisma): added a new model `JoinGroup`
    
    * feat(API): added Club President verification
    
    * feat(API): added delete club invitation data
    
    * feat(API): add `status` for `club.get.ts`
    
    * fix(typo): `joinGroup` in `group/delete.delete.ts`
    
    * feat: club.d.ts
    
    * feat(API): verify if `wechatGroupUrl` starts with certain trait
    
    * feat(API): verify if `wechatGroupUrl` starts with certain trait while updating the data
    
    * fix(API): use `parse` instead of `safeParse`
    
    * style(API): change several variable names
    
    * refactor(API): remove delete action for group
    
    * refactor(API): remove `club.d.ts`
    
    * refactor(API): rename `joinGroup` to `clubInfo`
    refactor: move `/cas/group` to `/cas/info`
    
    * fix(API): Local variable `clubInfo` is redundant
    
    * refactor(Prisma): model `JoinGroup` to `GroupInfo`
    
    * Update update.post.ts
    
    * Update new.post.ts
    
    * Update new.post.ts
    
    * Update update.post.ts
    
    ---------
    
    Co-authored-by: Alan Ye <45558679+AlanYe-Dev@users.noreply.github.com>
    Co-authored-by: Tom Tang <me@qwerzl.me>

---
+++ server/api/cas/info/get.get.ts
@@ -0,0 +1,30 @@
+import { PrismaClient } from '@prisma/client'
+import * as z from 'zod'
+
+const prisma = new PrismaClient()
+
+const requestSchema = z.object({
+  club: z.number(),
+})
+
+export default eventHandler(async (event) => {
+  const { auth } = event.context
+
+  if (!auth.userId) {
+    setResponseStatus(event, 403)
+    return
+  }
+
+  // get clubId from request body
+  const requestBody = await readValidatedBody(event, body => requestSchema.parse(body))
+
+  return await prisma.groupInfo.findUnique({
+    where: {
+      clubId: Number(requestBody.club),
+    },
+    // only use include if the value required is not a scalar
+    // include: {
+    //   wechatGroupUrl: true,
+    // },
+  })
+})
---
+++ server/api/cas/info/update.post.ts
@@ -0,0 +1,50 @@
+import { PrismaClient } from '@prisma/client'
+import * as z from 'zod'
+
+const prisma = new PrismaClient()
+
+const requestSchema = z.object({
+  clubId: z.number(),
+  wechatGroupUrl: z.string().startsWith('https://weixin.qq.com/g/', { message: 'WeChat Group URL required' }),
+  wechatGroupExpiration: z.string().datetime(),
+})
+
+export default eventHandler(async (event) => {
+  const { auth } = event.context
+
+  if (!auth.userId) {
+    setResponseStatus(event, 403)
+    return
+  }
+
+  const tsimsStudentId = (await prisma.user.findUniqueOrThrow({
+    where: {
+      id: auth.userId,
+    },
+  })).tsimsStudentId
+
+  const requestBody = await readValidatedBody(event, body => requestSchema.parse(body))
+
+  // check if the user is in the presidentByTsimsStudentId or vicePresidentByTsimsStudentId of the club
+  const club = await prisma.club.findFirst({
+    where: {
+      id: requestBody.clubId,
+      presidentByTsimsStudentId: tsimsStudentId,
+    },
+  })
+
+  if (!club) {
+    setResponseStatus(event, 404)
+    return
+  }
+
+  await prisma.groupInfo.update({
+    where: {
+      clubId: Number(requestBody.clubId),
+    },
+    data: {
+      wechatGroupUrl: requestBody.wechatGroupUrl,
+      wechatGroupExpiration: new Date(requestBody.wechatGroupExpiration),
+    },
+  })
+})
