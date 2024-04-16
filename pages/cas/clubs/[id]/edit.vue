a9f1cef18d874bcfca4f3d21ee089b51a2ed90ac
Alan Ye <45558679+AlanYe-Dev@users.noreply.github.com>
2024-04-17 00:59:53 +0800

    feat(clubInfo): show 404 when `isPresident` is false

--- pages/cas/clubs/[id]/edit.vue
+++ pages/cas/clubs/[id]/edit.vue
@@ -72,73 +72,108 @@ const onSubmit = handleSubmit(async (values) => {
   emit('refresh')
   resetForm()
 })
+
+// Get privilege information
+const { data: privilegeData } = await useFetch<{ isPresident: boolean }>('/api/cas/info/privilege', {
+  method: 'GET',
+  body: {
+    clubId: Number(id),
+  },
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+const isPresident = privilegeData.value?.isPresident || false
+
+if (isPresident)
+  title = 'Edit Club Info | Enspire'
+else
+  title = '404 Not Found | Enspire'
+
+useHead({
+  title,
+})
 </script>
 
 <template>
-  <Card class="w-full h-min mt-2">
-    <CardHeader>
-      <CardTitle v-if="noGroup" class="flex items-center h-min gap-x-1">
-        添加 {{ filteredClubs[0].groups[0].C_NameC }} 群聊信息
-      </CardTitle>
-      <CardTitle v-if="!noGroup" class="flex items-center h-min gap-x-1">
-        编辑 {{ filteredClubs[0].groups[0].C_NameC }} 群聊信息
-      </CardTitle>
-      <CardDescription class="flex items-center">
-        <Icon name="material-symbols:info-outline" />
-        <div class="ml-1">
-          {{ filteredClubs[0].groups[0].C_NameE }} Group Info
-        </div>
-      </CardDescription>
-    </CardHeader>
-    <CardContent>
-      <form class="space-y-6" @submit="onSubmit">
-        <FormField v-slot="{ componentField, value }" name="wechatGroupExpiration">
-          <FormItem class="flex flex-col">
-            <FormLabel>失效日期 Expire Date</FormLabel>
-            <Popover>
-              <PopoverTrigger as-child>
-                <FormControl>
-                  <Button
-                    :class="cn(
-                      'w-full ps-3 text-start font-normal',
-                      !value && 'text-muted-foreground',
-                    )" variant="outline"
-                    :disabled="isLoading"
-                  >
-                    <span>{{ value ? format(value, "PPP") : "选择日期..." }}</span>
-                    <Icon class="ms-auto opacity-50" name="material-symbols:calendar-today-outline" />
-                  </Button>
-                </FormControl>
-              </PopoverTrigger>
-              <PopoverContent class="p-0">
-                <Calendar :min-date="new Date()" v-bind="componentField" />
-              </PopoverContent>
-            </Popover>
-            <FormMessage />
-          </FormItem>
-        </FormField>
+  <div v-if="isPresident">
+    <Card class="w-full h-min mt-2">
+      <CardHeader>
+        <CardTitle v-if="noGroup" class="flex items-center h-min gap-x-1">
+          添加 {{ filteredClubs[0].groups[0].C_NameC }} 群聊信息
+        </CardTitle>
+        <CardTitle v-if="!noGroup" class="flex items-center h-min gap-x-1">
+          编辑 {{ filteredClubs[0].groups[0].C_NameC }} 群聊信息
+        </CardTitle>
+        <CardDescription class="flex items-center">
+          <Icon name="material-symbols:info-outline" />
+          <div class="ml-1">
+            {{ filteredClubs[0].groups[0].C_NameE }} Group Info
+          </div>
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form class="space-y-6" @submit="onSubmit">
+          <FormField v-slot="{ componentField, value }" name="wechatGroupExpiration">
+            <FormItem class="flex flex-col">
+              <FormLabel>失效日期 Expire Date</FormLabel>
+              <Popover>
+                <PopoverTrigger as-child>
+                  <FormControl>
+                    <Button
+                      :class="cn(
+                        'w-full ps-3 text-start font-normal',
+                        !value && 'text-muted-foreground',
+                      )" variant="outline"
+                      :disabled="isLoading"
+                    >
+                      <span>{{ value ? format(value, "PPP") : "选择日期..." }}</span>
+                      <Icon class="ms-auto opacity-50" name="material-symbols:calendar-today-outline" />
+                    </Button>
+                  </FormControl>
+                </PopoverTrigger>
+                <PopoverContent class="p-0">
+                  <Calendar :min-date="new Date()" v-bind="componentField" />
+                </PopoverContent>
+              </Popover>
+              <FormMessage />
+            </FormItem>
+          </FormField>
 
-        <FormField v-slot="{ componentField }" name="wechatGroupUrl">
-          <FormItem>
-            <FormLabel>WeChat Group URL</FormLabel>
-            <FormControl>
-              <Textarea
-                class="resize-none"
-                placeholder="WeChat Group URL"
-                v-bind="componentField"
-                :disabled="isLoading"
-              />
-            </FormControl>
-            <FormMessage />
-          </FormItem>
-        </FormField>
+          <FormField v-slot="{ componentField }" name="wechatGroupUrl">
+            <FormItem>
+              <FormLabel>WeChat Group URL</FormLabel>
+              <FormControl>
+                <Textarea
+                  class="resize-none"
+                  placeholder="WeChat Group URL"
+                  v-bind="componentField"
+                  :disabled="isLoading"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          </FormField>
 
-        <Button :disabled="isLoading" type="submit">
-          <Icon v-if="isLoading" class="mr-2" name="svg-spinners:180-ring-with-bg" />
-          提交
-        </Button>
-      </form>
-    </CardContent>
-  </Card>
-  <Toaster />
+          <Button :disabled="isLoading" type="submit">
+            <Icon v-if="isLoading" class="mr-2" name="svg-spinners:180-ring-with-bg" />
+            提交
+          </Button>
+        </form>
+      </CardContent>
+    </Card>
+    <Toaster />
+  </div>
+  <div v-else>
+    <div class="flex flex-col justify-center h-1/2 text-center">
+      <h3 class="font-bold text-xl">
+        You shouldn't be here... ヾ(≧へ≦)〃
+      </h3>
+      <br>
+      <NuxtLink class="w-full mt-2" to="/">
+        <Button>回到主页</Button>
+      </NuxtLink>
+    </div>
+  </div>
 </template>
