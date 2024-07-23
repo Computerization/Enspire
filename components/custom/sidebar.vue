9dce72d51584f2bb78a3cf0931b3910672a35dd7
qwerzl <me@qwerzl.me>
2024-07-23 10:00:05 +0800

    fix(sidebar): highlight criteria when route is `/manage/record`

--- components/custom/sidebar.vue
+++ components/custom/sidebar.vue
@@ -47,7 +47,7 @@ import { Button } from '@/components/ui/button'
             </Button>
           </NuxtLink>
           <NuxtLink to="/manage/record">
-            <Button :variant="$route.name === 'manage-leave' ? 'secondary' : 'ghost'" class="w-full justify-start mt-1">
+            <Button :variant="$route.name === 'manage-record' ? 'secondary' : 'ghost'" class="w-full justify-start mt-1">
               <Icon class="mr-2 h-4 w-4" name="charm:tick-double" />
               活动记录
             </Button>
