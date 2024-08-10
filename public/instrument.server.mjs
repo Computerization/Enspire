54004a3b5c6011076508bcdb1a7cf21034afbd66
qwerzl <me@qwerzl.me>
2024-08-10 22:44:26 +0800

    fix: sentry

---
+++ public/instrument.server.mjs
@@ -0,0 +1,8 @@
+import * as Sentry from '@sentry/nuxt';
+
+// Only run `init` when DSN is available
+if (process.env.SENTRY_DSN) {
+    Sentry.init({
+        dsn: process.env.SENTRY_DSN,
+    });
+}
