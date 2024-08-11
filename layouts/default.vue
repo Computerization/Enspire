85bfc94a3bee3158e1203d1009a052432c76c1cd
Alan Ye <git@wrye.dev>
2024-08-11 23:19:09 +0800

    feat: add `theme-color` for modern browser

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,6 +11,13 @@ useFavicon(favicon)
 const isSidebarOpen = ref(false)
 const route = useRoute()
 
+useHead({
+  meta: [
+    { name: 'theme-color', media: '(prefers-color-scheme: light)', content: 'white' },
+    { name: 'theme-color', media: '(prefers-color-scheme: dark)', content: '#1a202c' },
+  ],
+})
+
 function toggleSidebar() {
   isSidebarOpen.value = !isSidebarOpen.value
 }
