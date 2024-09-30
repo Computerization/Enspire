6ba6ac2d8bd3a94a0c25b730d734cb1e1a001fd5
qwerzl <me@qwerzl.me>
2024-09-30 11:48:08 +0800

    refactor(update-classroom-data): use upsert to update classroom data

---
+++ utils/update-classroom-data.ts
@@ -0,0 +1,30 @@
+import { PrismaClient } from '@prisma/client'
+import data from '~/data/classroom/classroom.json'
+
+const prisma = new PrismaClient()
+
+const runSequence: any = []
+
+data.forEach((e) => {
+  runSequence.push(
+    prisma.classroomData.upsert({
+      where: {
+        name: e.name,
+      },
+      update: {
+        name: e.name,
+        alias: e.alias,
+        size: e.size,
+      },
+      create: {
+        name: e.name,
+        alias: e.alias,
+        size: e.size,
+      },
+    }),
+  )
+})
+
+// eslint-disable-next-line no-console
+console.log(`start transaction with length ${runSequence.length}`)
+await prisma.$transaction(runSequence)
