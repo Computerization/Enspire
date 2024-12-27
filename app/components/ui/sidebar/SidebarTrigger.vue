fba46a8080562e47f76c5c443a48fdaff0586be8
qwerzl <me@qwerzl.me>
2024-12-27 17:38:46 +0800

    change `<SidebarTrigger />` icon

--- app/components/ui/sidebar/SidebarTrigger.vue
+++ app/components/ui/sidebar/SidebarTrigger.vue
@@ -20,7 +20,7 @@ const { toggleSidebar } = useSidebar()
     :class="cn('h-7 w-7', props.class)"
     @click="toggleSidebar"
   >
-    <PanelLeft />
+    <Icon name="lucide:panel-left" size="1.5em" />
     <span class="sr-only">Toggle Sidebar</span>
   </Button>
 </template>
