8136a15ecb3ada97d88ad9d7bc57e7f319e7e0c0
qwerzl <me@qwerzl.me>
2024-12-27 17:43:44 +0800

    sidebar blur background

--- app/components/ui/sheet/index.ts
+++ app/components/ui/sheet/index.ts
@@ -10,7 +10,7 @@ export { default as SheetTitle } from './SheetTitle.vue'
 export { default as SheetTrigger } from './SheetTrigger.vue'
 
 export const sheetVariants = cva(
-  'fixed z-50 gap-4 bg-background p-6 shadow-lg transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=open]:duration-500',
+  'fixed z-50 gap-4 bg-background p-6 shadow-lg transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=open]:duration-500 backdrop-blur-md',
   {
     variants: {
       side: {
